a5fa4bca054e595450dd4d71b2d453346c4c384b
zelalem <zelalem.amera.official@gmail.com>
2026-05-21 17:01:24 -0400

    added more testing

--- StressClassifier.py
+++ StressClassifier.py
@@ -16,48 +16,53 @@ class Network(nn.Module):
 	'''
 	neural network implementation for the above
 	'''
-	def __init__(self, parameters=None):
+	def __init__(self):
 		super().__init__()
+		print('initializing parameters')
+		self.cycles = nn.parameter.Parameter(torch.tensor(float(0)), requires_grad = False)
+		self.conv1 = nn.Conv1d(1,1,10,5)
+		self.conv2 = nn.Conv1d(1,1, 5, 3) 
+		self.conv3 = nn.Conv1d(1,1, 21, 2)
 		
-		if parameters:
-			print('loading in parameters')
-			self.load_state_dict(parameters)
-		else:
-			print('initializing parameters')
-			self.cycles = nn.parameter.Parameter(torch.tensor(float(0)), requires_grad = False)
-			self.conv1 = nn.Conv1d(1,1,10,5)
-			self.conv2 = nn.Conv1d(1,1, 5, 3) 
-			self.conv3 = nn.Conv1d(1,1, 21, 2)
+		self.feature_encoder_in = nn.parameter.Parameter(torch.rand((990, 90)) - 0.5, requires_grad = True)
+		self.feature_encoder_in_bias = nn.parameter.Parameter(torch.rand((990)) -0.5, requires_grad = True)
+		self.feature_encoder_hidden = nn.parameter.Parameter(torch.rand((990, 990) )- 0.5, requires_grad = True)
+		self.feature_encoder_hidden_bias = nn.parameter.Parameter(torch.rand((990) )- 0.5, requires_grad = True)
+		
+		self.combine_layer1 = nn.parameter.Parameter(torch.rand((990, 990 * 2)) - 0.5, requires_grad = True)
+		self.combine_layer1_bias = nn.parameter.Parameter(torch.rand((990)) - 0.5, requires_grad = True)
+		self.combine_layer2 = nn.parameter.Parameter(torch.rand((990, 990)) - 0.5, requires_grad = True)
+		self.combine_layer2_bias  = nn.parameter.Parameter(torch.rand((990)) - 0.5, requires_grad = True)
 	
-			self.attnlayer1 = nn.parameter.Parameter(torch.rand((2,1980), requires_grad = True))
-			self.attnlayer1_bias = nn.parameter.Parameter(torch.rand((2), requires_grad = True))
-			self.attnlayer2 = nn.parameter.Parameter(torch.rand((1,2), requires_grad = True))
-			self.attnlayer2_bias = nn.parameter.Parameter(torch.rand((1), requires_grad = True))
-			self.feature_weights = []
+		self.attnlayer1 = nn.parameter.Parameter(torch.rand((2, 990 * 2)) - 0.5, requires_grad = True)
+		self.attnlayer1_bias = nn.parameter.Parameter(torch.rand((2) )- 0.5, requires_grad = True)
+		self.attnlayer2 = nn.parameter.Parameter(torch.rand((1,2) )- 0.5, requires_grad = True)
+		self.attnlayer2_bias = nn.parameter.Parameter(torch.rand((1) )- 0.5, requires_grad = True)
+		self.feature_weights = []
 			
-			self.encode_in = nn.parameter.Parameter(torch.rand((500, 90), requires_grad = True))
-			self.encode_in_bias = nn.parameter.Parameter(torch.rand((500), requires_grad = True))
-			self.encode_hidden = nn.parameter.Parameter(torch.rand((500, 500), requires_grad = True))
-			self.encode_hidden_bias = nn.parameter.Parameter(torch.rand((500), requires_grad = True))
+		self.encode_in = nn.parameter.Parameter(torch.rand((500, 90) )- 0.5, requires_grad = True)
+		self.encode_in_bias = nn.parameter.Parameter(torch.rand((500) )- 0.5, requires_grad = True)
+		self.encode_hidden = nn.parameter.Parameter(torch.rand((500, 500)) - 0.5, requires_grad = True)
+		self.encode_hidden_bias = nn.parameter.Parameter(torch.rand((500)) - 0.5, requires_grad = True)
 	
-			self.recurrent_left_in = nn.parameter.Parameter(torch.rand((1000, 500), requires_grad = True))
-			self.recurrent_left_in_bias = nn.parameter.Parameter(torch.rand((1000), requires_grad = True))
-			self.recurrent_left_hidden = nn.parameter.Parameter(torch.rand((1000,1000), requires_grad = True))
-			self.recurrent_left_hidden_bias = nn.parameter.Parameter(torch.rand((1000), requires_grad = True))
+		self.recurrent_left_in = nn.parameter.Parameter(torch.rand((1000, 500) )-0.5, requires_grad = True)
+		self.recurrent_left_in_bias = nn.parameter.Parameter(torch.rand((1000) )-0.5, requires_grad = True)
+		self.recurrent_left_hidden = nn.parameter.Parameter(torch.rand((1000,1000)) -0.5, requires_grad = True)
+		self.recurrent_left_hidden_bias = nn.parameter.Parameter(torch.rand((1000)) -0.5, requires_grad = True)
 		
-			self.recurrent_right_in = nn.parameter.Parameter(torch.rand((1000,500), requires_grad = True))
-			self.recurrent_right_in_bias = nn.parameter.Parameter(torch.rand((1000), requires_grad = True))
-			self.recurrent_right_hidden = nn.parameter.Parameter(torch.rand((1000,1000), requires_grad = True))
-			self.recurrent_right_hidden_bias = nn.parameter.Parameter(torch.rand((1000), requires_grad = True))
+		self.recurrent_right_in = nn.parameter.Parameter(torch.rand((1000,500)) -0.5, requires_grad = True)
+		self.recurrent_right_in_bias = nn.parameter.Parameter(torch.rand((1000) )-0.5, requires_grad = True)
+		self.recurrent_right_hidden = nn.parameter.Parameter(torch.rand((1000,1000)) -0.5, requires_grad = True)
+		self.recurrent_right_hidden_bias = nn.parameter.Parameter(torch.rand((1000)) -0.5, requires_grad = True)
 		
-			self.recurrent_out1 = nn.parameter.Parameter(torch.rand((100, 2000), requires_grad = True))
-			self.recurrent_out1_bias = nn.parameter.Parameter(torch.rand((100), requires_grad = True))
-			self.recurrent_out2 = nn.parameter.Parameter(torch.rand((2, 100), requires_grad = True))
-			self.recurrent_out2_bias = nn.parameter.Parameter(torch.rand((2), requires_grad = True))
+		self.recurrent_out1 = nn.parameter.Parameter(torch.rand((100, 2000)) -0.5, requires_grad = True)
+		self.recurrent_out1_bias = nn.parameter.Parameter(torch.rand((100)) -0.5, requires_grad = True)
+		self.recurrent_out2 = nn.parameter.Parameter(torch.rand((2, 100) )-0.5, requires_grad = True)
+		self.recurrent_out2_bias = nn.parameter.Parameter(torch.rand((2)) -0.5, requires_grad = True)
 		
-			self.tanh = nn.Tanh()
-			self.sigmoid = nn.Sigmoid()
-			self.softmax = nn.Softmax(dim=-1)
+		self.tanh = nn.Tanh()
+		self.sigmoid = nn.Sigmoid()
+		self.softmax = nn.Softmax(dim=-1)
 					
 	def forward(self, word: Tensor, features: Tensor, debug=False):
 		'''
@@ -89,6 +94,7 @@ class Network(nn.Module):
 			print('vector encoded', word_encoding[-1].shape)
 		print('starting bi-directional recurrent network')
 		output = self.rnn_forward(torch.stack(word_encoding))
+		print('finished forward pass')
 		return output
 
 	def filter(self, features: Tensor, i: int):
@@ -112,8 +118,7 @@ class Network(nn.Module):
 		print('passing through vector')
 		for i in range(length):
 			input = syll[n: n + self.encode_in.shape[1]]
-			print('reading context', input.shape)
-			hidden = self.tanh(torch.matmul(self.encode_in, input) + self.encode_in_bias + torch.matmul(self.encode_hidden, prev) + self.encode_hidden_bias)
+			hidden = self.sigmoid(torch.matmul(self.encode_in, input) + self.encode_in_bias + torch.matmul(self.encode_hidden, prev) + self.encode_hidden_bias)
 			prev = hidden
 			n += self.encode_in.shape[1]
 		return prev
@@ -127,9 +132,9 @@ class Network(nn.Module):
 		output shape: (990)
 		'''
 		channel = input.reshape((1,input.shape[0]))
-		conv_first = self.tanh(self.conv1(channel))
-		conv_second = self.tanh(self.conv2(conv_first))
-		conv_third = self.tanh(self.conv3(conv_second))
+		conv_first = self.sigmoid(self.conv1(channel))
+		conv_second = self.sigmoid(self.conv2(conv_first))
+		conv_third = self.sigmoid(self.conv3(conv_second))
 		
 		return conv_third.reshape(-1)
 		
@@ -141,12 +146,45 @@ class Network(nn.Module):
 		'''
 		injected_list = []
 		for feature in features:
-			injected_list.append((embedding + feature))
+			encoded_feature = self.encode_features(feature)
+			combined = self.combine(embedding, encoded_feature)
+			injected_list.append(combined)
 		attended = self.attend(torch.stack(injected_list))
 		return attended
 		
-		
 	
+	def combine(self, embedding: Tensor, encoded_feature: Tensor):
+		'''
+		passes the two tensors through the combiner network and returns the result
+		input shape (990 * 2)
+		output shape (990)
+		'''
+		print('combining features and audio')
+		input = torch.cat((embedding, encoded_feature))
+		print(input.shape)
+		first_layer = self.sigmoid(torch.matmul(self.combine_layer1, input) + self.combine_layer1_bias)
+		second_layer = self.sigmoid(torch.matmul(self.combine_layer2, first_layer) + self.combine_layer2_bias)
+		print('combined')
+		print(second_layer.shape)
+		return second_layer
+		
+
+	def encode_features(self, feature: Tensor):
+		'''
+		passes the a feature encoder over the input tensor and returns the result
+		input shape (990)
+		output shape (990)
+		'''
+		prev = torch.ones((self.feature_encoder_hidden.shape[0],))
+		n = 0
+		length =  int(len(feature) / self.feature_encoder_in.shape[1])
+		print('encoding feature')
+		for i in range(length):
+			input = feature[n: n + self.feature_encoder_in.shape[1]]
+			hidden = self.sigmoid(torch.matmul(self.feature_encoder_in, input) + self.feature_encoder_in_bias + torch.matmul(self.feature_encoder_hidden, prev) + self.feature_encoder_hidden_bias)
+			prev = hidden
+			n += self.feature_encoder_in.shape[1]
+		return prev
 		
 	def attend(self, embedded_tensors: Tensor):
 		'''
@@ -182,7 +220,7 @@ class Network(nn.Module):
 		output shape: (1)
 		'''
 		input = torch.cat((attention_source, attention_target))
-		first_layer = self.tanh(torch.matmul( self.attnlayer1, input ) + self.attnlayer1_bias)
+		first_layer = self.sigmoid(torch.matmul( self.attnlayer1, input ) + self.attnlayer1_bias)
 		second_layer = self.sigmoid(torch.matmul(self.attnlayer2, first_layer ) + self.attnlayer2_bias)
 		return second_layer
 		
@@ -195,7 +233,7 @@ class Network(nn.Module):
 		prev = torch.ones((self.recurrent_left_hidden.shape[0]),)
 		hidden_list = []
 		for input in injected_list:
-			hidden = self.tanh(torch.matmul(self.recurrent_left_in, input) + self.recurrent_left_in_bias + torch.matmul(self.recurrent_left_hidden, prev) + self.recurrent_left_hidden_bias)
+			hidden = self.sigmoid(torch.matmul(self.recurrent_left_in, input) + self.recurrent_left_in_bias + torch.matmul(self.recurrent_left_hidden, prev) + self.recurrent_left_hidden_bias)
 			hidden_list.append(hidden)
 			prev = hidden
 		reverse_hidden_list = []
@@ -203,7 +241,7 @@ class Network(nn.Module):
 		n = len(injected_list) - 1
 		while n >= 0:
 			input = injected_list[n]
-			hidden = self.tanh(torch.matmul(self.recurrent_right_in, input) + self.recurrent_right_in_bias + torch.matmul(self.recurrent_right_hidden, prev) + self.recurrent_right_hidden_bias)
+			hidden = self.sigmoid(torch.matmul(self.recurrent_right_in, input) + self.recurrent_right_in_bias + torch.matmul(self.recurrent_right_hidden, prev) + self.recurrent_right_hidden_bias)
 			reverse_hidden_list.append(hidden)
 			prev = hidden
 			n -= 1
--- TestStressClassifier.py
+++ TestStressClassifier.py
@@ -9,7 +9,7 @@ import torch
 from torch import Tensor
 import pandas as pd
 
-def test(network: StressClassifier, input: list[Tensor], gold: list[Tensor], features: list[list[Tensor]])
+def test(network: StressClassifier, input: list, gold: list, features: list):
 	'''
 	this is the main function of the program. we pass the input words once through the network, and evaluate the results.
 	'''
@@ -25,43 +25,52 @@ def evaluate(netwok: StressClassifier, input_data: tuple):
 	'''
 	passes, the input data through the network and uses the output to write an evaluation for the model performance
 	'''
+	print('evaluating model performance')
 	input, gold, features = input_data
 	output = []
 	attention_weights = []
 	for i, word in enumerate(input):
-		f = filter(features, i)
-		y_hat = network.forward(word, f)
+		f = torch.Tensor(filter(features, i))
+		x = torch.Tensor(word)
+		print(f'sequentially passing word {i} to the network', x.shape, f.shape)
+		y_hat = network.forward(x, f)
 		output.append(y_hat)
-		attention_weights.append(network.attention_weights)
-	
-	cycles, syllables, recall, precision, fscore = network.cycles.item(), len(gold[0]), recall(output, gold), precision(output, gold), f_score(recall(output, gold), precision(output, gold))
-	write_results(cycles, syllables, acc, precision, fscore, 'dev')
+		attention_weights.append(network.feature_weights)
+	cycles, syllables = network.cycles.item(), len(gold[0]) 
+	r = recall(output, gold)
+	p = precision(output, gold)
+	f = fscore(r, p)
+	write_results(cycles, syllables, r, p, f, 'dev')
 	write_feature_weights(output, gold, attention_weights, 'dev')
 	
-def write_results(cycles:int, syllables: int, acc: float, precision:float, fscore: float, batch: str):
+def write_results(cycles:int, syllables: int, r: float, p:float, fs: float, batch: str):
 	'''
 	writes the result to the directory 
 	'''
-	path = (f'results/syllable_{syllables}/{batch}/model_performance.txt')
-	if path.exists()
+	print('writing result table')
+	path = Path(f'results/syllable_{syllables}/{batch}/model_performance.txt')
+	if path.exists():
 		with path.open(mode='a') as f:
-			line = f'{cycles}\t{syllables}\t{recall}\t{precision}\t{fscore}\n'
+			line = f'{cycles}\t{syllables}\t{r}\t{p}\t{fs}\n'
 			f.write(line)
 	else:
 		path.touch()
 		with path.open(mode='w') as f:
 			line = f'cycles\tsyllables\trecall\tprecision\tfscore\n'
 			f.write(line)
-			line = f'{cycles}\t{syllables}\t{recall}\t{precision}\{fscore}\n'
+			line = f'{cycles}\t{syllables}\t{r}\t{p}\t{fs}\n'
 			f.write(line)
 	
 def write_feature_weights(output: list[Tensor], gold: list[Tensor], attention_weights: list[list], batch: str):
 	'''
 	writes a file containing the model's performance on each syllable and the weights that it assigned to each syllable.
 	'''
+	print('writing feature analysis')
 	syllable = len(gold[0])
 	path = Path(f'results/syllable_{syllable}/{batch}/feature_analysis.txt')
-	table_path = next((Path(f'{batch}_data/syllable_{syllable}')).glob('**/*.csv))
+	print('writing to file', path)
+	table_path = next(Path(f'wavefiles_syllabified/syllable_{syllable}/{batch}').glob('*.csv'))
+	print('reading from table', table_path)
 	table = pd.read_csv(table_path)
 	ipa = table['ipa']
 	with path.open('w') as f:
@@ -70,65 +79,79 @@ def write_feature_weights(output: list[Tensor], gold: list[Tensor], attention_we
 		for i, ipa in enumerate(ipa):
 			for j in range(syllable):
 				if j == 0:
-					line = f'{ipa}\t{j + 1}\t{torch.round(output[i][j], decimals=2)}\t{gold[i][j]}\t{ gold[i][j] == torch.argmax(output[i][j])}\t{attention_weights[i][j]}\n
+					line = f'{ipa}\t{j + 1}\t{to_list(output[i][j])}\t{gold[i][j]}\t{ gold[i][j] == torch.argmax(output[i][j])}\t{attention_weights[i][j]}\n'
 					f.write(line)
 				else:
-					line = f'\t\t{j + 1}\t{torch.round(output[i][j], decimals=2)}\t{gold[i][j]}\t{ gold[i][j] == torch.argmax(output[i][j])}\t{attention_weights[i][j]}\n
+					line = f'\t{j + 1}\t{to_list(output[i][j])}\t{gold[i][j]}\t{ gold[i][j] == torch.argmax(output[i][j])}\t{attention_weights[i][j]}\n'
 					f.write(line)
 					
-def recall(output: list[Tensor],  gold: list[Tensor]):
-   '''
-   computes the recall of output against gold.
-   '''
-   y_hat = []
-   for word in output:
-   	classification_value = []
-   	for distribution in word:
-		classification_value.append(torch.argmax(distribution))
-	y_hat.append(torch.Tensor(classification_value))
-   recall = 0
-   for prediction, true in zip(y_hat, gold):
-   	i = torch.argmax(true)
-	if prediction[i] == 1:
-		recall +=1
-   return round(recall / len(gold), 2)
+def to_list(t: Tensor):
+	'''
+	converts this tensor to a list in a specific format that is desired for the output text file.
+	'''
+	
+	rounded = [round(v.item(), 2) for v in t]
+	return rounded
+	
+def recall(output: list[Tensor],  gold: list):
+	'''
+	computes the recall of output against gold.
+	'''
+	y_hat = []
+	for word in output:
+		classification_value = []
+		for distribution in word:
+			classification_value.append(torch.argmax(distribution))
+		y_hat.append(classification_value)
+	recall = 0
+	for prediction, true in zip(y_hat, gold):
+		i = torch.argmax(torch.tensor(true))
+		if prediction[i] == 1:
+			recall +=1
+	return round(recall / len(gold), 2)
    
-def precision(output: list[Tensor],  gold: list[Tensor]):
-    '''
-    computes the precision as the proportion of true positives to all positives
-    '''
-    y_hat = []
-   for word in output:
-   	classification_value = []
-   	for distribution in word:
-		classification_value.append(torch.argmax(distribution))
-	y_hat.append(torch.Tensor(classification_value))
-   precision = 0
-   total = 0
-   for prediction, true in zip(y_hat, gold):
-   	for i, val in enumerate(prediction):
-		if val == 1:
-			total +=1
-			if true[i] == 1:
-				precision+=1
-   return round(precision/total, 2)
+def precision(output: list[Tensor],  gold: list):
+	'''
+	computes the precision as the proportion of true positives to all positives
+	'''
+	y_hat = []
+	for word in output:
+		classification_value = []
+		for distribution in word:
+			classification_value.append(torch.argmax(distribution))
+			y_hat.append(classification_value)
+	precision = 0
+	total = 0
+	for prediction, true in zip(y_hat, gold):
+		for i, val in enumerate(prediction):
+			if val == 1:
+				total +=1
+				if true[i] == 1:
+					precision+=1
+	if total == 0:
+		return 0
+	return round(precision/total, 2)
+	
   
-  def fscore(recall: float, precision: float):
-  	'''
+def fscore(recall: float, precision: float):
+	'''
 	computes the fscore measure
 	'''
+	if recall + precision == 0:
+		return None
 	f = round(2 * (recall * precision)/ (recall + precision), 2)
-		
-def filter(features:list[list[Tensor]], i: int):
-	'''
-	returns the ith tensor of each list in features
-	'''
-	f = []
-	for feature in features:
-		f.append(feature[i])
 	return f
 		
-def split_by_syllable(input: list[Tensor], gold: list[Tensor], features: list[list[Tensor]], size_list: list[int])
+def filter(features:list, i: int):
+	'''
+	returns the ith element of each list in features
+	'''
+	f = []
+	for words in features:
+		f.append(words[i])
+	return f
+		
+def split_by_syllable(input: list, gold: list, features: list, size_list: list[int]):
 	'''
 	splits the input batch by syllables, each batch is always sorted by syllable size, size list contains the number of words in each batch. The result is returned as a triple.
 	'''
@@ -143,7 +166,7 @@ def split_by_syllable(input: list[Tensor], gold: list[Tensor], features: list[li
 	return (two_input, two_gold, two_features), (three_input, three_gold, three_features), (four_input, four_gold, four_features)
 	
 	
-def count_syllable_sizes(input: list[Tensor])
+def count_syllable_sizes(input: list):
 	'''
 	the input contains a list of words that are already sorted by syllable size. 
 	we count how many of them are there in each and return the result as a list
@@ -166,6 +189,7 @@ def read_in_data(batch: str):
 	'''
 	reads in input and gold data from the current directory, also reads in features
 	'''
+	print('reading in testing data')
 	input_path, gold_path, feature_path = Path(f'{batch}_data/input.json'), Path(f'{batch}_data/gold.json'), Path(f'{batch}_features.json')
 	input, gold, features = json.load(input_path.open(mode='r')), json.load(gold_path.open(mode='r')), json.load(feature_path.open(mode='r'))
 	
@@ -174,13 +198,13 @@ def read_in_data(batch: str):
 	features_tensor= []
 	
 	for word in input:
-		input_tensor.append(torch.Tensor(word)
+		input_tensor.append(word)
 	for word in gold:
-		gold_tensor.append(torch.Tensor(word))
+		gold_tensor.append(word)
 	for f in features:
 		word_list = []
 		for word in f:
-			word_list.append(torch.Tensor(word)
+			word_list.append(word)
 		features_tensor.append(word_list)
 	return input_tensor, gold_tensor, features_tensor
 
@@ -188,11 +212,15 @@ def load_model():
 	'''
 	loads in the trained model parameters and initializes the network for testing. 
 	'''
+	print('loading in model')
 	path = Path('model.json')
 	state_dict = json.load(path.open(mode='r'))
 	for key, value in state_dict.items():
-		state_dict[key] = torch.Tensor(value)
-	network = StressClassifier.Network(state_dict)
+		if key == 'cycles':
+			state_dict[key] = torch.tensor(value)
+		state_dict[key] = torch.nn.parameter.Parameter(torch.tensor(value), requires_grad = True)
+	network = StressClassifier.Network()
+	network.load_state_dict(state_dict)
 	return network
 
 if __name__== '__main__':
